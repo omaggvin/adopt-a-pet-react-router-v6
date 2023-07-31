6a4b96c8a94830cfdd65e628808fbdc66cd7af09
Olav Magnus Nevestad Aggvin <omaggvin@uia.no>
2023-07-31 15:50:51 +0200

    added full somewhat completed project to github

---
+++ src/components/search/index.js
@@ -0,0 +1,40 @@
+import React, { useRef } from 'react';
+// Import createSearchParams
+// Import useNavigate
+import { createSearchParams, useNavigate } from 'react-router-dom';
+
+const Search = () => {
+
+  // get navigate function
+  const navigate = useNavigate();
+  
+  const searchInputRef = useRef();
+
+  const onSearchHandler = (e) => {
+    e.preventDefault();
+
+    const searchQuery = {
+      name: searchInputRef.current.value
+    };
+
+    // use createSearchParams 
+    const query = createSearchParams(searchQuery);
+    
+navigate({
+  pathname: '/search',
+  search: `?${query}`  // notice you'll need the leading `?`
+});
+    // imperatively redirect with useNavigate() returned function
+  };
+
+  return (
+    <form onSubmit={onSearchHandler} className="search-form">
+      <input type="text" className="search" ref={searchInputRef} />
+      <button type="submit" className="search-button">
+        🔎
+      </button>
+    </form>
+  );
+};
+
+export default Search;
